f0a2e47aaeaf10e28d75614305071c97e8f6cbf0
ZengzeWang <2625533610@qq.com>
2021-01-25 17:16:00 +0800

    接口错误拦截

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <router-vire></router-vire>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import storage from "./storage/index";
 export default {
   name: "App",
   components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+    storage.setItem("abc", { a: 1 }, "user");
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router'
 
+//根据前端的跨域方式作调整
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+//接口错误拦截
+axios.interceptors.response.use(function (response) {
+  let res = response.data;//获取接口返回值
+  if (res.status == 0) {
+    return res.data;
+  } else if (res.status == 10) {
+    window.location.href = '/#/login';
+  } else {
+    alert(res.msg);
+  }
+});
+
+
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
