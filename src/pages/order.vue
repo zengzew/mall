6823131e4f883e0e8a319c00e644a6a0a24c3b3b
ZengzeWang <2625533610@qq.com>
2021-01-31 04:16:26 +0800

    完成订单页面

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    <nav-header></nav-header>
     <router-view></router-view>
+    <service-bar></service-bar>
     <nav-footer></nav-footer>
   </div>
 </template>
 <script>
+import ServiceBar from "./../components/ServiceBar";
+import NavFooter from "./../components/NavFooter";
 export default {
-  name: "nav-header",
+  name: "order",
+  data() {
+    return {
+      title: "",
+      tip: "",
+    };
+  },
+  components: {
+    ServiceBar,
+    NavFooter,
+  },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
     // outputDir:'dist',
     // indexPath:'index2.html',
     // lintOnSave:false,
-    productionSourceMap: false,
+    productionSourceMap: true,
     chainWebpack: (config) => {
         config.plugins.delete('prefetch');
     }
